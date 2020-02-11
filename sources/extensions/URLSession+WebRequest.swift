8fe601da9177476c64e65d057a05b95ae369d888
Alex Lin <alex_lin@perfectcorp.com>
2020-02-11 16:56:21 +0800

    1. add new interface for URLSession request with codable

--- sources/extensions/URLSession+WebRequest.swift
+++ sources/extensions/URLSession+WebRequest.swift
@@ -12,4 +12,12 @@ public extension URLSession {
     func dataTask(urlString:String, params:[String:Any] = [:], timeoutList:[Int] = [3, 6, 10, 30], method:RequestMethod = .post) -> WebRequest {
         return WebRequest(urlSession: self, urlString: urlString, parameters: params, timeoutList: timeoutList, method: method)
     }
+
+    func dataTask<T>(urlString:String, params:T?, timeoutList:[Int] = [3, 6, 10, 30], method:RequestMethod = .post) -> WebRequest where T:Encodable {
+        var dictParam: [String:Any] = [:]
+        if let codableParam = params, let data = try? JSONEncoder().encode(codableParam), let dict = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String:Any] {
+            dictParam = dict
+        }
+        return WebRequest(urlSession: self, urlString: urlString, parameters: dictParam, timeoutList: timeoutList, method: method)
+    }
 }
